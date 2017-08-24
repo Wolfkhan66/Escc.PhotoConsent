c15cb8d038087c8bcbb7ceda00d70fbeabc4093f
Cai Lehwald <cai.lehwald@eastsussex.gov.uk>
2017-08-24 13:17:34 +0100

    Implement Advance search function for Forms, added Search panel to Index and ManageForms Page

--- Escc.PhotoConsent/Controllers/AdminController.cs
+++ Escc.PhotoConsent/Controllers/AdminController.cs
@@ -3,7 +3,9 @@ using Escc.PhotoConsent.Models.ViewModels;
 using Escc.PhotoConsent.Services;
 using Escc.PhotoConsent.Services.Interfaces;
 using System;
+using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
@@ -29,30 +31,61 @@ namespace Escc.PhotoConsent.Controllers
         }
 
         [Route("ManageForms", Name = "ManageForms")]
-        public ActionResult ManageForms()
+        public ActionResult ManageForms(List<ConsentFormModel> Forms)
         {
             var model = new ManageFormViewModel();
-            var ConsentForms = _databaseService.GetConsentForms();
+            if (Forms == null)
+            {
+                Forms = _databaseService.GetConsentForms();
+            }
+            model.Forms.Table = PrepareFormsTable(Forms);
 
-            model.Forms.Table = new DataTable();
-            model.Forms.Table.Columns.Add("ID", typeof(string));
-            model.Forms.Table.Columns.Add("Project Reference", typeof(string));
-            model.Forms.Table.Columns.Add("Created By", typeof(string));
-            model.Forms.Table.Columns.Add("Date Created", typeof(string));
-            model.Forms.Table.Columns.Add("Consent", typeof(HtmlString));
-            model.Forms.Table.Columns.Add("View", typeof(HtmlString));
+            return View("ManageForms", model);
+        }
 
-            foreach (var Form in ConsentForms)
+        public DataTable PrepareFormsTable(List<ConsentFormModel> Forms)
+        {
+            var Table = new DataTable();
+            Table.Columns.Add("ID", typeof(string));
+            Table.Columns.Add("Project Reference", typeof(string));
+            Table.Columns.Add("Created By", typeof(string));
+            Table.Columns.Add("Date Created", typeof(string));
+            Table.Columns.Add("Consent", typeof(HtmlString));
+            Table.Columns.Add("View", typeof(HtmlString));
+
+            foreach (var Form in Forms)
             {
 
                 var Consent = Form.ConsentGiven ? new HtmlString("<span class=\"glyphicon glyphicon-ok text-success\" aria-hidden=\"true\"></span>") : new HtmlString("<span class=\"glyphicon glyphicon-remove text-danger\" aria-hidden=\"true\"></span></div>");
                 var ViewLink = new HtmlString(string.Format("<a type=\"button\" class=\"btn btn-primary btn-sm\" href=\"ViewForm/{0}\">View</a>", Form.FormID));
 
-                model.Forms.Table.Rows.Add(Form.FormID, Form.ProjectReference, Form.CreatedBy, Form.DateCreated, Consent, ViewLink);
+                Table.Rows.Add(Form.FormID, Form.ProjectReference, Form.CreatedBy, Form.DateCreated, Consent, ViewLink);
             }
-            
+            return Table;
+        }
 
-            return View(model);
+        public ActionResult SearchForms(string ProjectReference, string CreatedBy, string DateCreated, string Consent)
+        {
+            var ConsentForms = _databaseService.GetConsentForms();
+
+            if (ProjectReference != "")
+            {
+                ConsentForms = ConsentForms.Where(x => x.ProjectReference.ToLower() == ProjectReference.ToLower()).ToList();
+            }
+            if (CreatedBy != "")
+            {
+                ConsentForms = ConsentForms.Where(x => x.CreatedBy.ToLower() == CreatedBy.ToLower()).ToList();
+            }
+            if (DateCreated != "")
+            {
+                ConsentForms = ConsentForms.Where(x => x.DateCreated.ToString().Split(' ')[0] == DateCreated).ToList();
+            }
+            if (Consent != "" || Consent != "Both")
+            {
+                bool consentGiven = Consent.ToLower() == "true" ? true : false;
+                ConsentForms = ConsentForms.Where(x => x.ConsentGiven == consentGiven).ToList();
+            }
+           return ManageForms(ConsentForms);
         }
 
         [HttpPost]
